368e0e43c3060614cdb3c28ecc4afba8c1fb7806
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-06-08 07:45:31 +0430

    Done dicee project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,59 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
+  return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice'),
+          title: const Text('DICEE'),
+          backgroundColor: Colors.blue,
         ),
-        body: MyApp(),
+        body: Home(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Random number generator
+  static int getRandom() => Random().nextInt(5) + 1;
+
+  int first_player = 1;
+  int seconde_player = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextButton(
+              style: TextButton.styleFrom(),
+              child: Image.asset("images/dice$seconde_player.png", width: 100),
+              onPressed: () => setState(() => seconde_player = getRandom()),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextButton(
+              style: TextButton.styleFrom(),
+              child: Image.asset("images/dice$first_player.png", width: 100),
+              onPressed: () => setState(() => first_player = getRandom()),
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
